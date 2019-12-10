140643ec35abe3b6f3d8a314d416762af5c8e655
mahendra-singh-rathore <mahendra.itm89@gmail.com>
2019-12-10 18:38:53 +0530

    Project Intializer

---
+++ ClothOnlineShopping web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothOnlineShopping_web.Startup))]
+namespace ClothOnlineShopping_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
